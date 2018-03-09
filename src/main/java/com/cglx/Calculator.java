ecff1336de5d61ede7f165ee465a7555d5f4573c
Rani_Hirave <rani.hirave@cognologix.com>
2018-03-09 14:30:22 +0530

    add function console removed

--- src/main/java/com/cglx/Calculator.java
+++ src/main/java/com/cglx/Calculator.java
@@ -7,8 +7,7 @@ public class Calculator {
 
     public int add(int a, int b)
     {
-    	System.out.println("add");
-        return a+b;
+    	return a+b;
     }
 
     public int multiply(int a, int b)
