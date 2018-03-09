e5fe886e27ff9ab64a1efd7f51e3ab5861dc4be3
Rani_Hirave <rani.hirave@cognologix.com>
2018-03-09 14:01:42 +0530

    console added

--- src/main/java/com/cglx/Calculator.java
+++ src/main/java/com/cglx/Calculator.java
@@ -7,34 +7,40 @@ public class Calculator {
 
     public int add(int a, int b)
     {
-    	System.out.println("Rani");
+    	System.out.println("add");
         return a+b;
     }
 
     public int multiply(int a, int b)
     {
+    	System.out.println("multiply");
         return a*b;
     }
 
     public  int divide(int a, int b)
     {
+    	System.out.println("divide");
         return a/b;
     }
 
     public int substract(int a, int b)
     {
+    	System.out.println("substract");
         return a - b;
     }
 
     public int square(int aa)
     {
+    	System.out.println("square");
         return aa*aa;
     }
 
     public boolean areEqual(int f11, int f12) {
+    	System.out.println("equal");
         return f11 == f12;
     }
     public boolean areNotEqual(int f11, int f12) {
+    	System.out.println("not equal");
         return f11 != f12;
     }
 }
